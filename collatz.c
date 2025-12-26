9daadb7ef6757706af7e9dd2044255a8b221ff29
Gianluca Mazzini <gianluca@mazzini.org>
2025-12-26 05:31:50 +0100

    Fix formatting and improve Collatz function output

--- collatz.c
+++ collatz.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-void collatz(long long n,long long *max, long *len){
+void collatz(long long n,long long *max, long long *len){
   *max=0; *len=0;
   for(;;){
     if(n==1)return;
     if(n%2==0)n=n/2;
     else n=3*n+1;
-    *len++;
+    (*len)++;
     if(n>*max)*max=n;
   }
 }
 
 int main(){
-  long long n,max;
-  long len;
+  long long n,max,len;
 
   for(n=1;n<100;n++){
    collatz(n,&max,&len);
-   printf("%lld,%lld,%ld\n",n,max,len);
+   printf("%lld,%lld,%lld\n",n,max,len);
   } 
 }
