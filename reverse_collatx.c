71923a14c478566aaca587706ea962d51cff0d65
Gianluca Mazzini <gianluca@mazzini.org>
2026-03-06 10:25:58 +0100

    Add reverse_collatx.c with Collatz sequence logic
    Implement reverse collatz function to print sequence.

---
+++ reverse_collatx.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cc(long long n){
+  printf("%lld ",n);
+  if((n-1)%3==0)cc((n-1)/3);
+  else cc(n*2);
+}
+
+int main(int argc,char *argv[]){
+  long long t,n;
+  t=atoll(argv[1]);
+  cc(1);
+}
