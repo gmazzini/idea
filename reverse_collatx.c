072deb0e6c2d45752438d7df5e0fc95e2358cf7c
Gianluca Mazzini <gianluca@mazzini.org>
2026-03-06 10:36:59 +0100

    Modify cc function to include depth parameter

--- reverse_collatx.c
+++ reverse_collatx.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cc(long long n){
+void cc(long long n,long p){
   printf("%lld ",n);
-  if((n-1)%3==0)cc((n-1)/3);
-  cc(n*2);
+  if(p==10)return;
+  cc(n*2,p+1);
+  if((n-1)%3==0)cc((n-1)/3,p+1);
 }
 
 int main(int argc,char *argv[]){
   long long t,n;
   t=atoll(argv[1]);
-  cc(1);
+  cc(1,1);
 }
