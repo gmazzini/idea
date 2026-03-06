c68d4dc50cb4689e74988af924c6df3a5510b8e9
Gianluca Mazzini <gianluca@mazzini.org>
2026-03-06 10:58:26 +0100

    Update reverse_collatx.c

--- reverse_collatx.c
+++ reverse_collatx.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+long pp;
+
 void cc(long long n,long p){
   printf("%lld,%ld ",n,p);
-  if(p==40)return;
+  if(p==pp)return;
   cc(n*2,p+1);
   if(n>4 && (n-1)%3==0)cc((n-1)/3,p+1);
 }
 
 int main(int argc,char *argv[]){
-  long long t,n;
-  t=atoll(argv[1]);
+  pp=atol(argv[1]);
   cc(1,1);
 }
