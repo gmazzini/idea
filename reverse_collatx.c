8e0cd890671ea9e63ee75d2a48c8b07616e8983d
Gianluca Mazzini <gianluca@mazzini.org>
2026-03-06 10:46:09 +0100

    Add condition to check n > 4 before recursive call

--- reverse_collatx.c
+++ reverse_collatx.c
@@ -5,7 +5,7 @@ void cc(long long n,long p){
   printf("%lld,%ld ",n,p);
   if(p==10)return;
   cc(n*2,p+1);
-  if((n-1)%3==0)cc((n-1)/3,p+1);
+  if(n>4 && (n-1)%3==0)cc((n-1)/3,p+1);
 }
 
 int main(int argc,char *argv[]){
